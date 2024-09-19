d889dd5fb09d336a6c9cea0f7908b6630e96ff56
Gemma Fernandez <gemmazuera@gmail.com>
2024-09-19 14:55:17 +0200

    iteracion 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import {Routes, Route} from "react-router-dom"
 
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+          <Route
+            path={"/StudentsDetailsPage"}
+            element={<StudentDetailsPage />}
+          />
+          <Route path={"/UserProfilePage"} element={<UserProfilePage />} />
+        </Routes>
       </div>
-
     </div>
   );
 }
